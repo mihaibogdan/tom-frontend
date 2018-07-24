d79ff16eb733f04b707bcdc624fa2235a912c72e
Bogdan Mihai <bogdan@non.ro>
2018-07-24 16:46:13 +0300

    implemented first login screen

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from './login/components/login.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent},
+
+  { path: '**', redirectTo: 'login' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
+  providers: []
+})
+export class AppRoutingModule { }
