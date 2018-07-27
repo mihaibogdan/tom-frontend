ac836f8de12771586d67acb83d80e5eda40eda00
Bogdan Mihai <bogdan@non.ro>
2018-07-27 18:22:58 +0300

    implemented profile design

--- src/app/user/components/settings/settings.component.ts
+++ src/app/user/components/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-user-settings',
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
+  @ViewChild('nameInput') nameInput:ElementRef;
+  @ViewChild('emailInput') emailInput:ElementRef;
+
+  public inlineEditing = {};
+  public user = {
+    name: 'John Doe',
+    email: 'johndoe@test.com'
+  };
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  public toggleInline = (type) => {
+    this.inlineEditing[type] = !this.inlineEditing[type];
+  };
+
+  public updateUser = (form, property) => {
+    if (!form.valid) return;
+
+    if (!this.inlineEditing[property) {
+      this.toggleInline(property);
+      this.focusOnInput(property);
+    } else {
+      //todo: API Call to update user
+      this.toggleInline(property);
+    }
+  };
+
+  public focusOnInput = (input) => {
+    setTimeout(() => {
+      this[`${input}Input`].nativeElement.focus();
+    });
+  }
 }
