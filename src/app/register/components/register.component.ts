141867f7ed70942105372209ae1c663e00271199
Bogdan Mihai <bogdan@non.ro>
2018-07-31 21:01:50 +0300

    added messages on register

--- src/app/register/components/register.component.ts
+++ src/app/register/components/register.component.ts
@@ -7,6 +7,8 @@ import { FadeInOrOut } from '../../shared/animations/animations';
 
 import { AuthService } from '../../shared/services/auth.service';
 
+import * as _ from 'lodash';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -15,28 +17,38 @@ import { AuthService } from '../../shared/services/auth.service';
 })
 export class RegisterComponent implements OnInit {
   private jwtHelper = new JwtHelperService();
-  public user: User = {};
+  public user:User = {};
   public showForm = true;
-  private errors = {}
+  private errors = {};
+  public isLoading = false;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService:AuthService, private router:Router) {
+  }
 
   ngOnInit() {
   }
 
   public register = (form) => {
-    if (!form.valid) return ;
+    if (!form.valid) return;
+
+    this.isLoading = true;
+    this.errors = {};
 
     this.authService.register(form.value)
       .then(() => {
         let data = this.jwtHelper.decodeToken(this.authService.getAccessToken());
 
+        this.isLoading = false;
         if (data && data.role) {
           this.router.navigate([data.role]);
         }
       })
       .catch(err => {
-        this.errors = err.errors;
+        this.isLoading = false;
+
+        this.errors = _.transform(err.errors, (result, err) => {
+          result[err.field] = _.isArray(err.messages) ? err.messages[0] : err.messages;
+        }, {});
       });
   }
 }
